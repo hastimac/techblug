1325bc852b4e0ccfb8d7a58d83ae0200b2f775c6
hastimac <hastimakkarizadeh@gmail.com>
2025-08-29 17:22:30 +0330

    spalshscreen created

---
+++ lib/splash.dart
@@ -0,0 +1,30 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class splashScreen extends StatefulWidget {
+  @override
+  State<splashScreen> createState() => _splashScreenState();
+}
+
+class _splashScreenState extends State<splashScreen> {
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 2));
+    super.initState();
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(150),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Image(image: AssetImage('assets/images/logo.png'))],
+        ),
+      ),
+    );
+  }
+}
